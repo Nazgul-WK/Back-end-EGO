8567afdf2a7671d5cd3a5588f45aaf391a204637
Nazgul-WK <egor.kondrov@gmail.com>
2023-04-21 02:47:51 +0500

    4 commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, bro")
+class Calculator:
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def minus(self):
+        return self.a - self.b
+
+    def sub(self):
+        return self.a * self.b
